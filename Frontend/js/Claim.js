42d7d55fa1c68b76fdf0957e70e52f5bcca0e613
Muhammad-Ibrahim-Amjad64 <123ibrahim7890@gmail.com>
2024-01-12 15:34:49 +0500

    1-12-2024

--- Frontend/js/Claim.js
+++ Frontend/js/Claim.js
@@ -1,67 +1,92 @@
 /** @format */
 
-if (typeof window.ethereum !== 'undefined') {
-  const connectWalletButton = document.getElementById('connectWalletButton');
-  const walletAddressElement = document.getElementById('walletAddress');
-  const twtr = document.getElementById('twitter_line');
-  const twitterLin = document.getElementById('twitterLink');
 
-  connectWalletButton.style.display = 'none';
+if (typeof window.ethereum !== "undefined") {
+  console.log("iske andar aa rha ha ");
 
-  twitterLin.addEventListener('click', () => {
-    connectWalletButton.style.display = 'inline-block';
-    window.open(twitterLin.href, '_blank');
+  const connectWalletButton = document.getElementById("connectWalletButton");
+  const walletAddressElement = document.getElementById("walletAddress");
+  const twtr = document.getElementById("twitter_line");
+  const twitterLin = document.getElementById("twitterLink");
+
+  // connectWalletButton.style.display = 'none';
+
+  twitterLin.addEventListener("click", () => {
+    connectWalletButton.style.display = "inline-block";
+    window.open(twitterLin.href, "_blank");
   });
 
-  connectWalletButton.addEventListener('click', async () => {
+  connectWalletButton.addEventListener("click", async () => {
     try {
       let account;
       const accounts = await window.ethereum.request({
-        method: 'eth_requestAccounts',
+        method: "eth_requestAccounts",
       });
       account = accounts[0];
 
       const networkId = await window.ethereum.request({
-        method: 'net_version',
+        method: "net_version",
       });
 
       // Check if the connected wallet is on BSC (networkId '56')
-      if (networkId !== '56') {
-        alert('Please connect a Binance Smart Chain wallet.');
+      if (networkId !== "56") {
+        alert("Please connect a Binance Smart Chain wallet.");
         return;
       }
 
       alert(`Connected to wallet!\nSelected account: ${account}`);
       console.log(account);
-      twtr.style.display = 'none';
-      connectWalletButton.style.display = 'none';
+      twtr.style.display = "none";
+      connectWalletButton.style.display = "none";
       walletAddressElement.textContent = `Connected Wallet: ${account}`;
-      walletAddressElement.style.display = 'inline-block';
+      walletAddressElement.style.display = "inline-block";
 
       // Use bnb_getBalance to get BNB balance on BSC
       const balanceResult = await window.ethereum.request({
-        method: 'eth_getBalance',
-        params: [account, 'latest'],
+        method: "eth_getBalance",
+        params: [account, "latest"],
       });
       console.log(balanceResult);
       let wei = parseInt(balanceResult, 16);
 
       // Convert BNB balance from Wei to BNB
       let balance = wei / 10 ** 18;
-      console.log(balance + ' BNB');
+      console.log(balance + " BNB");
       alert(`Smart contract interaction successful! Balance: ${balance} BNB`);
+
+      // DISCONNECT Logics
+      const logoutBtn = document.getElementById("logout-user");
+      if (logoutBtn) {
+        logoutBtn.addEventListener("click", function () {
+          localStorage.removeItem("loginid");
+          const user = document.getElementById("user-modal");
+          user.style.display = "none";
+          const buttonContainer = document.getElementById("AuthContainer");
+          buttonContainer.style.display = "block";
+          window.location.href = "/";
+        });
+      }
+
+      // removing claim modal
+      const clainForm = document.getElementById("claim-modal");
+      clainForm.style.display = "none";
+
+      // removing user icon
+      const user = document.getElementById("user-modal");
+      user.style.display = "none";
+
+      // active logout btn
+      const logout = document.getElementById("logout-user");
+      logout.style.display = "block";
+      // active address
+      const userAddress = document.getElementById("user-address");
+      userAddress.style.display = "block";
     } catch (error) {
-      console.error('Error connecting to wallet:', error.message);
+      console.error("Error connecting to wallet:", error.message);
     }
   });
 } else {
   alert(
-    'MetaMask or another compatible wallet is not detected. Please install MetaMask or use a compatible wallet to connect.'
+    "MetaMask or another compatible wallet is not detected. Please install MetaMask or use a compatible wallet to connect."
   );
 }
-
-
-
-
-
-
